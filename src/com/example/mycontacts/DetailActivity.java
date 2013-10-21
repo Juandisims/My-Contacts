dc6c7707afd24103112305e35347fc28511b36dc
Roberto Rodríguez Echeverría <rrecheve@gmail.com>
2013-10-21 16:52:43 +0200

    Revert "SQLite DB and CRUD operations. Custom cursors."
    This reverts commit 1cc88be9656c49fd5244e484861acf705e3d6f8b.

--- src/com/example/mycontacts/DetailActivity.java
+++ src/com/example/mycontacts/DetailActivity.java
@@ -1,20 +1,15 @@
 package com.example.mycontacts;
 
+import com.example.mycontacts.R;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import com.example.mycontacts.MyContactsDataBase.ContactDetailCursor;
-
 //Ejemplo de actividad que usa fragmentos para componer su vista
 public class DetailActivity extends Activity {
-
-	private Integer contact_id;
-	private MyContactsDataBase contactsDB;
-	private ContactDetailCursor contact;
 
 	//Sobreescribir onCreate para cargar vista y configurar activity
 	@Override
@@ -24,19 +19,10 @@ public class DetailActivity extends Activity {
 		//establecer como vista el layout indicado
 		setContentView(R.layout.activity_detail);
 		
-		// get the job_id for this job from the bundle passed by MicroJobsList
-        Bundle bIn = this.getIntent().getExtras();
-        contact_id = Integer.valueOf(bIn.getInt("_id"));
-
-        contactsDB = new MyContactsDataBase(this);
-        contact = contactsDB.getContactDetails(contact_id.longValue());
-        startManagingCursor(contact);
-		
 		TextView text01 = (TextView) findViewById(R.id.textView01);
-		text01.setText(contact.getColName());
+		text01.setText("Perico");
 		TextView text02 = (TextView) findViewById(R.id.textView02);
-		text02.setText(contact.getColSurname());
-		
+		text02.setText("Palotes");
 		//configurar Action Bar
 		setupActionBar();
 	}
@@ -67,12 +53,15 @@ public class DetailActivity extends Activity {
 		switch (item.getItemId()) {
 		
 		case R.id.action_delete: //id que hemos dado a la accion de ajustes
-			contactsDB.deleteContact(contact_id);
-		    finish();
+			// Remplazar el fragmento original del FrameLayout 'fragment_container' por el de las preferencias
+			//Obtener gestor, crear transaccion, indicar tipo de transaccion (replace) y realizar transaccion (commit)
+		
 			return true;
 
 		case android.R.id.home: //id del sistema que identifica el boton Arriba (Up)
-			NavUtils.navigateUpFromSameTask(this);
+			//No podemos usar NavUtils por usar fragmentos en el activity
+			//Finalizamos la actividad 
+			finish();
 			return true;
 
 		default:
